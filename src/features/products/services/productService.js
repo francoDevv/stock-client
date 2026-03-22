8aae5b7ceac6ed1b09bca4280506b38329e4ecfc
Franco Votta <votta.franco16@gmail.com>
2026-03-21 23:46:25 -0300

    fix API_URL

--- src/features/products/services/productService.js
+++ src/features/products/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/products";
+https://stock-api-nps0.onrender.com/products
 
 export const getProducts = async () => {
     const response = await axios.get(API_URL);
